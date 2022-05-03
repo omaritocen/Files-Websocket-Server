1dae19d03382ed673c1d447d6112eb4f7fdd8246
omaritocen <omaralaaabdelkader@gmail.com>
2022-05-03 03:08:44 +0200

    basic server, client code

--- client.py
+++ client.py
@@ -1,1 +1,24 @@
-print("Hello from client")
+import socket
+
+from constants import * 
+
+# Initiate client socket
+clientSocket = socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(SERVER_ADDRESS)
+
+# Get sentence from user
+sentence = input('Input lowercase sentence:')
+
+# Send the message to the server
+clientSocket.send(sentence.encode())
+
+
+# Decode recieved socket
+recieved_sentence = clientSocket.recv(BUFFER_SIZE)
+decoded_sentence = recieved_sentence.decode()
+
+# Print the result
+print('From server: ', decoded_sentence)
+
+# Close the connection
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,1 +1,33 @@
-print("Hello from sever")
+import socket
+import threading
+
+from constants import *
+
+## Define constants
+
+# Get the host IP address of the current machine
+ADDRESS = ('', PORT)
+
+# Setup Server
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS)
+
+
+def handle_client(conn, sender_address):
+    print(f'[NEW CONNECTION] recieved message from {sender_address}')
+
+def start():
+    server.listen()
+    print(f'[LISTENING] Server is Listening on {ADDRESS}')
+    while True:
+        
+        # Blocking wait for user connection
+        conn, sender_addrsss = server.accept()
+
+        # Delegate new connection to the worker
+        thread = threading.Thread(target=handle_client, args=(conn, sender_addrsss))
+        thread.start()
+
+        # Printing the total connections which equals to all threads - main 
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
