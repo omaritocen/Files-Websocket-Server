961f86b29176936c6ab0e712937314a5237a786e
Muhhamed Sallam <m.sallam2622000@gmail.com>
2022-05-03 13:49:25 +0200

    made input file replace sent message with GET/POST

--- client.py
+++ client.py
@@ -2,15 +2,22 @@ import socket
 
 from constants import * 
 
+# Read GET/POST requests from input file 
+with open('input_file.txt') as f:
+    requests = []
+    for line in f:
+        requests.append(line)
+
 # Initiate client socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect(SERVER_ADDRESS)
 
-# Get sentence from user
-sentence = input('Input lowercase sentence:')
+# Declaring GET/POST Requests
+GET = requests[0]
+POST = requests[1]
 
-# Send the message to the server
-clientSocket.send(sentence.encode())
+# Send the request to the server
+clientSocket.send(GET.encode())
 
 
 # Decode recieved socket
---
+++ test.py
@@ -0,0 +1,6 @@
+with open('input_file.txt') as f:
+    lines = []
+    for line in f:
+        lines.append(line)
+
+print(lines[1])        
