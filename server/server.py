148e972b8dc5678d543251b69bde0a8dd406f3cd
omaritocen <omaralaaabdelkader@gmail.com>
2022-05-13 01:30:52 +0200

    refactored server to use content-length

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ MAX_CONNECTIONS = 5
 PORT = 5505
 BUFFER_SIZE = 4096
 FORMAT = "utf-8"
- 
+
 # Setup Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = socket.gethostbyname("localhost")
@@ -18,73 +18,80 @@ ADDRESS = (HOST, PORT)
 server.bind(ADDRESS)
 print(server.getsockname())
 
+
 def recvall(conn):
-    full_request = b""
-    c = 0
-    while True:
-        readable, writeable, exceptional = select.select([conn], [], [], 1)
-        if conn in readable:
-            request = conn.recv(BUFFER_SIZE)
-        else: break
-        if not request:
-            break
-        print(f"{c}: {request}")
-        c += 1
-        full_request += request
+    request = conn.recv(BUFFER_SIZE)
 
-    lines = full_request.split(b"\r\n")
-    words = lines[0].split(b" ")
-    request_type = words[0].decode()
-    filename = words[1].split(b'/')[-1]
-    http_type = words[2].decode()
-    ext = filename.split(b".")[1]
-    header = full_request.split(b"\r\n\r\n")[0]
-    print(header.decode())
+    headers = request.split(b"\r\n\r\n")
+    header_lines = headers[0].split(b"\r\n")
 
+    status_line = header_lines[0].decode().split(" ")
+    request_type = status_line[0]
+    filename = status_line[1].split('/')[-1]
+    ext = filename.split(".")[1]
+    http_type = status_line[2]
+
+    # IN CASE OF GET
     if request_type == 'GET':
-        file = transfer_file(filename.decode())
+        file = transfer_file(filename)
         if file != -1:
             response = rg.get_response_by_verb(http_type, request_type, True, file)
-             # Send data back to client
-            conn.send(response)
+
         else:
             response = rg.get_response_by_verb(http_type, request_type, False)
-            # Send data back to client
-            conn.send(response)
 
+        # Send data back to client
+        conn.send(response)
+
+
+    # IN CASE OF POST
     else:
-        #handle images
-        if(ext.decode()=="jpg" or ext.decode()=="png"):
-            image = full_request.split(b"\r\n\r\n")[1].split(b"\r\n")[0]
-            receive = receive_file(filename.decode(),image)
+
+        content_length_line = [x for x in header_lines if x.startswith(b'Content-Length')]
+        content_length = int(content_length_line[0].split(b" ")[1].decode())
+        remaining_content = content_length - len(headers[1])
+
+        body = headers[1]
+
+        headers_length = len(headers[0])
+        if content_length >= BUFFER_SIZE - headers_length:
+            while True:
+                request = conn.recv(BUFFER_SIZE)
+                if not request:
+                    break
+                body += request
+
+                remaining_content -= BUFFER_SIZE
+                if remaining_content <= 0:
+                    break
+
+        # handle images
+        if ext == "jpg" or ext == "png":
+            image = body
+            receive = receive_file(filename, image)
             if receive != -1:
                 response = rg.get_response_by_verb(http_type, request_type, True)
-                conn.send(response.encode(FORMAT))
             else:
                 response = rg.get_response_by_verb(http_type, request_type, False)
-                conn.send(response.encode(FORMAT))
-        #handle txt and html        
+        # handle txt and html
         else:
-            data = lines[-2].decode()
-            print(data)
-            receive = receive_file(filename.decode(), data)
+            data = body.decode()
+            receive = receive_file(filename, data)
             if receive != -1:
                 response = rg.get_response_by_verb(http_type, request_type, True)
-                conn.send(response.encode(FORMAT))
             else:
                 response = rg.get_response_by_verb(http_type, request_type, False)
-                conn.send(response.encode(FORMAT))
+
+        conn.send(response.encode(FORMAT))
 
 
-
- 
-def transfer_file(filename :str):
+def transfer_file(filename: str):
     try:
         ext = filename.split('.')[1]
-        if(ext=='jpg' or ext=='png'):
-           file = open(filename, "rb")
-           return file.read()
-        else:    
+        if ext == 'jpg' or ext == 'png':
+            file = open(filename, "rb")
+            return file.read()
+        else:
             file = open(filename, "r")
             return file.read().encode(FORMAT)
     except FileNotFoundError:
@@ -97,53 +104,50 @@ def transfer_file(filename :str):
         print(f"Unexpected Error: {sys.exc_info()[0]}")
         return -1
 
- 
+
 def receive_file(filename, data):
     try:
         ext = filename.split('.')[1]
-        if(ext=='jpg' or ext=='png'):
-           file = open(filename, "wb")
-           file.write(data)
-        else:   
-           file = open(filename, "w")
-           file.write(data)
-           return 0
+        if (ext == 'jpg' or ext == 'png'):
+            file = open(filename, "wb")
+            file.write(data)
+        else:
+            file = open(filename, "w")
+            file.write(data)
+            return 0
     except IOError as e:
         print(f"IOError: {e}")
         return -1
     except:
         print(f"Unexpected Error: {sys.exc_info()[0]}")
         return -1
- 
- 
+
+
 def handle_client(conn, sender_address):
-    
     print(f'[NEW CONNECTION] recieved message from {sender_address}')
- 
+
     # Recieve data from connection
     recvall(conn)
-  
+
     # Close client connection
     print(f"[CLOSE CONNECTION] client: {sender_address}")
     conn.close()
 
 
- 
 def start():
     server.listen(MAX_CONNECTIONS)
     print(f'[LISTENING] Server is Listening on {ADDRESS}')
     while True:
- 
         # Blocking wait for user connection
         conn, sender_addrsss = server.accept()
- 
+
         # Delegate new connection to the worker
         thread = threading.Thread(target=handle_client, args=(conn, sender_addrsss))
         thread.start()
- 
+
         # Printing the total connections which equals to all threads - main 
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
- 
- 
+
+
 print('[STARTING] SERVER IS STARTING')
 start()
